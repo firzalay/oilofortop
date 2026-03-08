cf9d3721cccf40ef4c9519b3f0971c47112493e7
firzalay <firza7004@gmail.com>
2026-03-08 20:36:48 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,98 @@
+import { useEffect, useRef, useState } from "react";
+import { images } from "./assets/images";
+import Draggable from "react-draggable";
+
+function App() {
+    const [open, setOpen] = useState(false);
+    const draggableRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleEscape = (event: KeyboardEvent) => {
+            if (event.key === "Escape") {
+                setOpen(!open);
+            }
+        };
+
+        window.addEventListener("keydown", handleEscape);
+
+        return () => {
+            window.removeEventListener("keydown", handleEscape);
+        };
+    }, [open]);
+
+    return (
+        <>
+            <div className="bg-[#f7f4f4]">
+                <button className="absolute outline-none" onClick={() => setOpen(!open)}>
+                    <img
+                        src={images.escImage}
+                        alt=""
+                        className="w-35 h-35 translate-y-2 transition delay-150 duration-300 cursor-pointer hover:translate-y-0 lg:invisible"
+                    />
+                </button>
+
+                <section className="min-h-screen container mx-auto px-8 flex justify-center items-center flex-col">
+                    <nav className="fixed inset-0">
+                        <ul className="flex justify-center items-center gap-5 font-sans-serif p-4 invisible lg:visible">
+                            <li className="">
+                                <a href="">home_</a>
+                            </li>   
+                            <li>
+                                <a href="">about_</a>
+                            </li>
+                            <li>
+                                <a href="">portofolio_</a>
+                            </li>
+                            <li>
+                                <a href="">contact me_</a>
+                            </li>
+                        </ul>
+                    </nav>
+
+                    <Draggable nodeRef={draggableRef}>
+                        <div ref={draggableRef}>
+                            <img className="h-40 w-40 cursor-grabbing" draggable={false} src={images.img5} alt="" />
+                        </div>
+                    </Draggable>
+
+                    <p className="font-serif italic text-3xl text-center">
+                        <span className="font-sans-serif">Hi!</span> I'm Fatahillah Firzalay
+                    </p>
+                    <p className="mt-2 font-sans-serif underline">web dev</p>
+                    <p className="text-sm mt-2 font-light font-sans-serif tracking-tight text-center">
+                        A <span className="bg-[#3167ce] text-white">professionally</span> lazy person, I build things
+                        fast so I can enjoy being lazy <span className="bg-[#3167ce] text-white">person</span> later.
+                    </p>
+
+                    <div className="mt-4 flex gap-2">
+                        <img src={images.discordLogo} alt="" className="w-6 h-6 hover:cursor-pointer" />
+                        <img src={images.instagramLogo} alt="" className="w-6 h-6 hover:cursor-pointer" />
+                        <img src={images.musicLogo} alt="" className="w-6 h-6 hover:cursor-pointer" />
+                    </div>
+
+                    <div
+                        className={`bg-[#3167ce] font-sans-serif min-h-screen container mx-auto absolute ${open ? "opacity-100 visible" : "invisible"}`}
+                        onClick={() => setOpen(!open)}
+                    >
+                        <p className="text-white text-center text-2xl mt-5">----- MENU -----</p>
+
+                        <div className="text-center">
+                            <h1 className="text-white text-6xl mt-8 hover:bg-white hover:text-[#3167ce] hover:cursor-pointer">
+                                ABOUT ME
+                            </h1>
+                            <h1 className="text-white text-6xl mt-8 hover:bg-white hover:text-[#3167ce] hover:cursor-pointer">
+                                PORTOFOLIO
+                            </h1>
+                            <h1 className="text-white text-6xl mt-8 hover:bg-white hover:text-[#3167ce] hover:cursor-pointer">
+                                CONTACT ME
+                            </h1>
+                            <p className="text-white mt-8">Press esc key to continue...</p>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </>
+    );
+}
+
+export default App;
---
+++ src/assets/images.ts
@@ -0,0 +1,26 @@
+import img1 from "./images/img-1.png";
+import img2 from "./images/img-2.png";
+import img3 from "./images/img-3.png";
+import img4 from "./images/img-4.png";
+import img5 from "./images/img-5.png";
+import img6 from "./images/img-6.png";
+import img7 from "./images/img-7.png";
+import instagramLogo from "./images/instagram.png";
+import discordLogo from "./images/discord.png";
+import musicLogo from "./images/music.png";
+import escImage from "./images/esc.png";
+
+
+export const images = {
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  instagramLogo,
+  discordLogo,
+  musicLogo,
+  escImage
+};
